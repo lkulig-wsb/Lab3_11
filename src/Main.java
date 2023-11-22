40b634c742c88001f9c6b2d61426e41be5cfa4e8
lkulig-wsb <lkulig@student.wsb-nlu.edu.pl>
2023-11-22 17:11:30 +0100

    Second Commit.

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,58 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        System.out.println("\nRandom number");
+        System.out.println("--------------");
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        double randomNumber = Math.random();
+
+        System.out.println("Random double from 0.00 to 1.00: " + randomNumber);
+        int randomInt = (int)(randomNumber*20);
+        System.out.println("Random int from 0 to 20: " + randomInt);
+
+        System.out.println();
+        //TablicaJednowymiarowa[] - wypełnienie jednowymiarowej tablicy 20ma elementami
+        //wygenerowanymi losowo, z przedziału od 0 do 20.
+        int[] TablicaJednowymiarowa = new int [20];
+        for(int i = 0; i< TablicaJednowymiarowa.length; i++)
+        {
+            TablicaJednowymiarowa[i]=(int)(Math.random()*20);;
         }
+
+        //Wyświetlenie elementów tablicy TablicaJednowymiarowa[]
+        System.out.println("TablicaJednowymiarowa: ");
+        System.out.println("----------------------------");
+
+        for(int value: TablicaJednowymiarowa)
+        {
+            System.out.print(value + " ");
+
+        }
+
+        //TablicaDwuwymiarowa[][] - wypełnienie dwuwymiarowej tablicy 20ma elementami
+        //wygenerowanymi losowo, z przedziału od 0 do 20.
+        int[][] TablicaDwuwymiarowa = new int[4][5];
+        for(int i = 0; i < TablicaDwuwymiarowa.length; i++)
+        {
+            for(int j = 0; j < TablicaDwuwymiarowa[i].length; j++)
+            {
+                TablicaDwuwymiarowa[i][j] = (int)(Math.random()*20);
+            }
+        }
+
+        //Wyświetlenie elementów tablicy TablicaDwuwymiarowa[][]
+        System.out.println("\n\nTablicaDwuwymiarowa[][]:");
+        System.out.println("-------------------------");
+        for(int i = 0; i < TablicaDwuwymiarowa.length; i++)
+        {
+            for(int j = 0; j < TablicaDwuwymiarowa[i].length; j++)
+            {
+                System.out.print(TablicaDwuwymiarowa[i][j] + "\t");
+            }
+            System.out.println();
+        }
+        System.out.println("\n");
     }
+
 }
